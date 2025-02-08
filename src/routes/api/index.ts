ce2d01da63a0eba1101f8071a295b96457b22379
Jason Cable <cablej02@gmail.com>
2025-02-08 00:16:03 -0600

    updated db connection and setup route structure

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-
 const router = Router();
+import userRouter from './userRoutes.js';
+import thoughtRouter from './thoughtRoutes.js';
 
-// router.use('/users', userRouter);
-// router.use('/thoughts', thoughtsRouter);
+router.use('/users', userRouter);
+router.use('/thoughts', thoughtRouter);
 
 export default router;
---
+++ src/routes/api/thoughtRoutes.ts
@@ -0,0 +1,9 @@
+// import Thought from '../../models/Thought';
+import { Router, Request,Response } from 'express';
+const router = Router();
+
+router.route('/').get((_req: Request, res: Response) => {
+    res.json({ message: 'Hello, world!' });
+});
+
+export default router;
