515feeef796f2e9ab4097ccd9ba9562e920f2360
Jason Cable <cablej02@gmail.com>
2025-02-08 23:44:30 -0600

    add and remove reactions to thoughtController and routes.  Updated createThought to also add it tothe user's thoughts array

--- src/controllers/thoughtController.ts
+++ src/controllers/thoughtController.ts
@@ -1,4 +1,5 @@
 import Thought from '../models/Thought.js';
+import User from '../models/User.js';
 import { Request, Response } from 'express';
 
 export const getAllThoughts = async (_req: Request, res: Response) => {
@@ -28,7 +29,19 @@ export const getSingleThought = async (req: Request, res: Response) => {
 export const createThought = async (req: Request, res: Response) => {
     try {
         const thought = await Thought.create(req.body);
-        res.json(thought);
+
+        // add thought to user's thoughts array
+        const user = await User.findByIdAndUpdate(
+            req.body.userId,
+            { $push: { thoughts: thought._id } },
+            { new: true }
+        );
+        
+        if(!user) {
+            res.status(404).json({ message: 'User not found.' });
+        } else{
+            res.json(thought);
+        }
     } catch (err) {
         res.status(500).json({ message: `Failed to create thought. ${err}` });
     }
@@ -49,5 +62,41 @@ export const deleteThought = async (req: Request, res: Response) => {
         res.json(thought);
     } catch (err) {
         res.status(500).json({ message: `Failed to delete thought. ${err}` });
+    }
+};
+
+export const addReaction = async (req: Request, res: Response) => {
+    try {
+        const thought = await Thought.findByIdAndUpdate(
+            req.params.thoughtId,
+            { $push: { reactions: req.body } },
+            { new: true }
+        );
+
+        if(!thought) {
+            res.status(404).json({ message: 'Thought not found.' });
+        } else {
+            res.json(thought);
+        }
+    } catch (err) {
+        res.status(500).json({ message: `Failed to add reaction. ${err}` });
+    }
+};
+
+export const removeReaction = async (req: Request, res: Response) => {
+    try {
+        const thought = await Thought.findByIdAndUpdate(
+            req.params.thoughtId,
+            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+            { new: true }
+        );
+
+        if(!thought) {
+            res.status(404).json({ message: 'Thought not found.' });
+        } else {
+            res.json(thought);
+        }
+    } catch (err) {
+        res.status(500).json({ message: `Failed to remove reaction. ${err}` });
     }
 };
--- src/routes/api/thoughtRoutes.ts
+++ src/routes/api/thoughtRoutes.ts
@@ -2,12 +2,18 @@ import { Router } from 'express';
 const router = Router();
 
 // import controller
-import { getAllThoughts, getSingleThought, createThought, updateThought, deleteThought } from '../../controllers/thoughtController.js';
+import { getAllThoughts, getSingleThought, createThought, updateThought, deleteThought, addReaction, removeReaction } from '../../controllers/thoughtController.js';
 
 // /api/thoughts
 router.route('/').get(getAllThoughts).post(createThought);
 
 // /api/thoughts/:id
 router.route('/:id').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 export default router;
